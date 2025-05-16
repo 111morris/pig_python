20a3ef24250a6753b2294d0d1dfbcbbab846ed16
mulandi <mulandimorris1@gmail.com>
2025-05-16 18:50:10 +0300

    dvpkg

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
     for player_idx in range(players):
-        print("\nPlayer number", player_idx+1, "turn as just started!\n")
+        print("\nPlayer number", player_idx+1, "turn as just started!")
         print("Your total score is:", player_scores[player_idx], "\n")
         current_score = 0
         
@@ -43,4 +43,7 @@ while max(player_scores) < max_score:
             print("You score is: ", current_score)
         player_scores[player_idx] += current_score
         print("your total score is: ", player_scores[player_idx])
+max_score = max(player_scores)
 
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx+1, "is the winner with the score of", )
